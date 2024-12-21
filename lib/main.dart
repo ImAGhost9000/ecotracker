e2c554f1c5c79bac0982d474e87c86546896e019
Kirby Bonita <bonita.kirbyy@gmail.com>
2024-12-21 14:15:47 +0800

    Much updated Ecotracker

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
